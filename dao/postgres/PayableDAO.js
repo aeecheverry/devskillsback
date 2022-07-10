5f4c8d9954f5f8258468cfc5dad0ecabe84ad8c9
aeecheverry <aeecheverry@uninorte.edu.co>
2022-07-10 00:39:34 -0500

    Update PayableDAO

--- dao/postgres/PayableDAO.js
+++ dao/postgres/PayableDAO.js
@@ -15,6 +15,16 @@ exports.PayableDAO = class PayableDAO {
         }
     }
 
+    async update(ticket) {
+        try{
+            const  client = await DBConnectionFactory.getConnection();
+            const data = await client.models.Ticket.update(ticket, { where: { bar_code: ticket.bar_code } });
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
     async list({ service_type }) {
         try{
             const client = await DBConnectionFactory.getConnection();
@@ -22,7 +32,7 @@ exports.PayableDAO = class PayableDAO {
                 attributes: ["service_type", "expiration_date", "amount", "bar_code"],
             };
             if(service_type) {
-                query.where = { service_type };
+                query.where = { service_type, pay_status: "pending" };
                 const index = query.attributes.indexOf("service_type");
                 if(index > -1) {
                     query.attributes.splice(index, 1);
